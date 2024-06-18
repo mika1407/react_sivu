45a19cfa75f8038021c590733f199fffffb62525
mika tiihonen <koneautomaatio@hotmail.com>
2024-06-18 13:12:13 +0300

    header päivitys

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -8,10 +8,10 @@ function Header(){
         <h1>My website</h1>
         <nav>
             <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/about">About</a></li>
-              <li><a href="/services">Services</a></li>
-              <li><a href="/contact">Contact</a></li>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/services">Services</Link></li>
+              <li><Link to="/contact">Contact</Link></li>
             </ul>
         </nav>
         <hr></hr>
